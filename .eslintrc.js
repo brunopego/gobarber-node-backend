713fc53522156291181de3d7bfcbf4643518a6de
Bruno Lacerda <brunolcpg@gmail.com>
2020-02-03 22:21:54 -0300

    Add prettier + eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,10 @@ module.exports = {
     node: true,
   },
   extends: [
-    'airbnb-base',
+    'airbnb-base', 'prettier',
+  ],
+  plugins: [
+    'prettier',
   ],
   globals: {
     Atomics: 'readonly',
@@ -15,5 +18,10 @@ module.exports = {
     sourceType: 'module',
   },
   rules: {
+    "prettier/prettier": "error",
+    "class-method-use-this": "off",
+    "no-param-reassign": "off",
+    "camelcase": "off",
+    "no-unused-vars": ["error", {"argsIgnorePattern": "next"}]
   },
 };
--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,6 @@ import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-    return res.json({ message: "Hello" });
-});
+routes.get('/', (req, res) => res.json({ message: 'Hello' }));
 
 export default routes;
