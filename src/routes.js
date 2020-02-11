59fdb1f7945b72ddf70067ab9b94455a2dd32840
Bruno Lacerda <brunolcpg@gmail.com>
2020-02-10 23:42:38 -0300

    Add user update and auth middleware

--- src/routes.js
+++ src/routes.js
@@ -4,9 +4,16 @@ import UserController from './app/controllers/UserController';
 
 import SessionController from './app/controllers/SessionController';
 
+import authMiddleware from './app/midddlewares/auth';
+
 const routes = new Router();
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
+
+// Global Middleware
+routes.use(authMiddleware);
+// Only the route below will use the auth middleware
+routes.put('/users', UserController.update);
 
 export default routes;
