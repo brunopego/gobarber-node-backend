31e825d366ba8e1af8576989170b04e99c5a66c9
Bruno Lacerda <brunolcpg@gmail.com>
2020-02-29 17:51:32 -0300

    Add schedule controller to list provider appoint

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 import authMiddleware from './app/midddlewares/auth';
 
@@ -26,6 +27,8 @@ routes.post('/appointments', AppointmentController.store);
 routes.post('/files', upload.single('file'), FileController.store);
 
 routes.get('/providers', ProviderController.index);
+
+routes.get('/schedule', ScheduleController.index);
 
 routes.put('/users', UserController.update);
 
