c7253c82330a9409f438bb5436674c4a34dca4f8
Bruno Lacerda <brunolcpg@gmail.com>
2020-03-01 17:42:26 -0300

    Add email config using mailtrap.io

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '3c6407b7a48375',
+    pass: '56b2e3c33bfdd7',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
