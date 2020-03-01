db49884b2294470f132a617d5b722665ba34bf96
Bruno Lacerda <brunolcpg@gmail.com>
2020-03-01 16:23:01 -0300

    Add notification controller to notify providers

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,7 @@ import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
 import ScheduleController from './app/controllers/ScheduleController';
+import NotificationController from './app/controllers/NotificationController';
 
 import authMiddleware from './app/midddlewares/auth';
 
@@ -25,6 +26,8 @@ routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
 
 routes.post('/files', upload.single('file'), FileController.store);
+
+routes.get('/notifications', NotificationController.index);
 
 routes.get('/providers', ProviderController.index);
 
